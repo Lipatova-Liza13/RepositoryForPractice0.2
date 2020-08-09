6894e90dfe88a19fe35deb257b5fe30763012925
Lipatova-Liza13 <lipatovaliza13@gmail.com>
2020-08-09 13:13:58 +0300

    add base project

---
+++ SimpleProjectForRepository/SimpleProjectForRepository/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  SimpleProjectForRepository
+//
+//  Created by Teslenko Anastasiya on 08.08.2020.
+//  Copyright © 2020 Liza Lipatova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var label: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
+
